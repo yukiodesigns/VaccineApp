207feabafaec3a5aeb8dea34a56a7ec27aa2383d
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-21 09:01:38 +0300

    Made Layout inflater

--- app/src/main/java/com/example/vaccinesapp/VaccineAdapter.kt
+++ app/src/main/java/com/example/vaccinesapp/VaccineAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.vaccinesapp
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class VaccineAdapter(val itemList: ArrayList<VaccineModel>): RecyclerView.Adapter<>() {
+class VaccineAdapter(val itemList: ArrayList<VaccineModel>): RecyclerView.Adapter<VaccineAdapter.MyViewHOlder>() {
 
     inner class MyViewHOlder(itemView: View): RecyclerView.ViewHolder(itemView){
         lateinit var vaccineImage: ImageView
@@ -16,5 +17,19 @@ class VaccineAdapter(val itemList: ArrayList<VaccineModel>): RecyclerView.Adapte
             vaccineImage = itemView.findViewById(R.id.image)
             vaccineTitle = itemView.findViewById(R.id.txt1)
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHOlder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return MyViewHOlder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHOlder, position: Int) {
+        holder.vaccineTitle.setText(itemList[position].name)
+        holder.vaccineImage.setImageResource(itemList[position].img)
     }
 }
